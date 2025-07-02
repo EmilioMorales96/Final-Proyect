bb27ea2cb1a0f7bb8491f216c554de2dd2f96d8a
Emilio <sergioemilio85@gmail.com>
2025-07-02 00:05:18 -0600

    backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
-console.log('DB config:', process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, process.env.DB_HOST);
+console.log('DB config:', process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_HOST);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -9,8 +9,14 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: 'mysql',
+    port: process.env.DB_PORT, 
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 );
 
--- backend/models/index.js
+++ backend/models/index.js
@@ -1,4 +1,22 @@
-import sequelize from '../config/db.js';
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "postgres", 
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 import UserModel from './user.model.js';
 import FormModel from './form.model.js';
