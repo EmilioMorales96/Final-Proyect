e9797920043b7faaad5455dee2f4f669b8ce487d
Emilio <sergioemilio85@gmail.com>
2025-07-02 02:12:03 -0600

    nothing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,8 @@ import Welcome from "./pages/Welcome";
 import { Toaster } from "react-hot-toast";
 import TemplateEditor from "./pages/TemplateEditor.jsx";
 
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
