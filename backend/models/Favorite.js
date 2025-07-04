ae079d0618b9afe43d74203625c4b0718b247e61
Emilio <sergioemilio85@gmail.com>
2025-07-04 00:27:25 -0600

    favorite fix

--- backend/models/Favorite.js
+++ backend/models/Favorite.js
@@ -1,4 +1,12 @@
 export default (sequelize, DataTypes) => {
   const Favorite = sequelize.define("Favorite", {}, { timestamps: false });
+  Favorite.associate = (models) => {
+    Favorite.belongsToMany(models.User, {
+      through: "UserFavorites",
+      as: "FavoredBy",
+      foreignKey: "favoriteId",
+      otherKey: "userId",
+    });
+  };
   return Favorite;
 };
