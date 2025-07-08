255019e7c52d7fa4c26d8a06883598e353d1d535
Emilio <sergioemilio85@gmail.com>
2025-07-08 00:36:34 -0600

    Fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { AppSettingsProvider } from "./context/AppSettingsContext.jsx";
 import "./index.css";
@@ -13,17 +13,18 @@ import AdminFormsPage from "./pages/admin/AdminFormsPage.jsx";
 
 console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <AppSettingsProvider>
         <BrowserRouter>
           <Toaster position="top-right" toastOptions={{ duration: 4000 }} />
-          <AppRoutes />
-          <Route path="/admin" element={<AdminDashboard />} />
-          <Route path="/admin/users" element={<AdminUsersPage />} />
-          <Route path="/admin/forms" element={<AdminFormsPage />} />
+          <Routes>
+            <Route path="/" element={<AppRoutes />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/admin/users" element={<AdminUsersPage />} />
+            <Route path="/admin/forms" element={<AdminFormsPage />} />
+          </Routes>
         </BrowserRouter>
       </AppSettingsProvider>
     </AuthProvider>
